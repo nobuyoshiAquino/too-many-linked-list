b5f47d5c92dbcf31aebae068f817f5c705d296de
Nobuyoshi Aquino <nobu-aquino@pm.me>
2022-01-13 18:52:00 -0300

    add immutable singly-linked list with shared ownership

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
 pub mod first;
 pub mod second;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod third;
